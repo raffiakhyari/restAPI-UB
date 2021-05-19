43e57280c21f8d882150efee04d749dd5b8e3cd0
Axel Christiant Kusuma <axelchristiant33@gmail.com>
2021-05-19 23:18:24 +0700

    nambahin migration dulu hehe

---
+++ database/migrations/2021_05_19_160726_create_order_detail_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->id();
+            $table->integer("produk_id");
+            $table->integer("jumlah");
+            $table->integer("harga");
+            $table->integer("order_id");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_detail');
+    }
+}
